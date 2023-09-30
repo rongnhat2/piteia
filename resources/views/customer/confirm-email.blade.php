462663a14913413446e8f560dd4f1d2c174b318c
Nguyen Nhat Long <long.nguyen2198@gmail.com>
2023-10-01 03:25:21 +0700

    update data

--- resources/views/customer/confirm-email.blade.php
+++ resources/views/customer/confirm-email.blade.php
@@ -2,11 +2,11 @@
 <br>
 <p>ラン アイン 様</p>
 <br>
-<p>SBTC Shop. でございます。</p>
+<p>Piteia Shop. でございます。</p>
 <p>この度は会員登録依頼をいただきましてまことに有り難うございます。</p>
 <p>ショッピングをお楽しみくださいませ。</p>
 <br>
-<p>今後ともどうぞ SBTC Shop. をよろしくお願い申し上げます。</p>
+<p>今後ともどうぞ Piteia Shop. をよろしくお願い申し上げます。</p>
 <br>
 <br>
 <p>Click <a href="{{ $url }}">Here</a> to active account.</p>
